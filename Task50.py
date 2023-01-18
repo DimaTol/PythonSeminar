b4c263f58ec71a75dee2f5cee28686f3367a0654
DimaTol <d.tolkachenko@ya.ru>
2023-01-18 15:43:11 +0300

    Тел справочник

--- Task50.py
+++ Task50.py
@@ -5,3 +5,39 @@
 # Программа должна сохранять данные в текстовом файле
 # Пользователь может ввести одну из характеристик для поиска определенной записи(Например имя или фамилию человека)
 # Использование функций. Ваша программа не должна быть линейной
+
+# data = open('tel.txt', 'r+')
+# data.writelines("Hello world" + "\n")
+
+
+# print (data.readline())
+# data.close ()
+       
+
+fileName = 'tel.txt'
+
+
+def writeFile(file_name):
+    with open(file_name, 'a') as data:
+        data.writelines("Hello world111" + '\n')
+
+
+def readFile(file_name):
+    result = []
+    with open(file_name, 'r+') as data:
+        for line in data:
+            result.append(line.split())
+    return result
+
+
+def findUsers(userList):
+    name = 'Ivan,'
+    for user in userList:
+        if user[1] == name:
+            print(user[3])
+
+
+# writeFile(fileName)
+print(type(readFile(fileName)))
+print(readFile(fileName))
+findUsers(readFile(fileName))
